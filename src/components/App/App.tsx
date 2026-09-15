1abbe602389bfc4f6fcce226ee0cbf8232dcc2d9
Artem Kovalchuk <byzone28@gmail.com>
2026-09-15 08:07:18 +0300

    2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,31 @@
-import styles from './App.module.css'
+import css from './App.module.css'
+import { CafeInfo } from '../CafeInfo/CafeInfo'
+import { VoteOptions } from '../VoteOptions/VoteOptions'
+import type { Votes } from '../../types/votes'
+import type { VoteType } from '../../types/votes'
 
+
+function handleVote(votes: VoteType) {
+  votes[vote] +=1
+}
+
+function resetVotes() {
+  votes.good = 0;
+  votes.neutral = 0;
+  votes.bad = 0;
+}
 
 function App() {
-  return (
-    <div className={styles.app}>
 
-    </div>
+
+  return (
+<div className={css.app}>
+  <CafeInfo />
+  <VoteOptions/>
+
+
+</div>
+
   )
 }
 
---
+++ src/types/votes.ts
@@ -0,0 +1,8 @@
+interface Votes {
+    good: number;
+    neutral: number;
+    bad: number;
+}
+type VoteType = 'good' | 'neutral' | 'bad';
+
+export type { Votes, VoteType };
